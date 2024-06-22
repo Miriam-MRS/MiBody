36f818c6dee4458901f710d579e0a61dc9e16eb3
Miriam PLESA <62135475+Miriam-MRS@users.noreply.github.com>
2024-06-22 15:03:27 +0300

    Update config.php config - actions - test

--- config.php
+++ config.php
@@ -1,2 +1,13 @@
 <?php
-$databaseURL = "";
+$databaseURL = "https://mibody-86533-default-rtdb.europe-west1.firebasedatabase.app/";
+include("firebaseRDB.php");
+$db = new firebaseRDB($databaseURL);
+$insert = $db->insert("user", [
+                      "userName": "Miriam",
+                      "userEmail": "plesamiriam@yahoo.com",
+                      "userPassword": "1234567"
+                      ]);
+$data = $db->retrieve("user");
+$data = json_decode($data, 1);
+print_r($data);
+

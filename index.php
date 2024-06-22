6938ca8f23a5fda410f73faf9662c0cbdf832726
Miriam PLESA <62135475+Miriam-MRS@users.noreply.github.com>
2024-06-22 17:31:22 +0300

    Update index.php signup call at init

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include("functions.php");
-
+$db = new firebaseRDB($databaseURL);
+signup($db, 1, "Alexandru", "alexandru@yahoo.com", "1234");
 ?>
 
 <!DOCTYPE html>

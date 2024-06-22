8aac631e5fc163cc0244887c6f8a60e6f2b6341b
Miriam PLESA <62135475+Miriam-MRS@users.noreply.github.com>
2024-06-22 18:08:52 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,58 +5,8 @@ ini_set('display_errors', 1);
 $databaseURL = "https://mibody-86533-default-rtdb.europe-west1.firebasedatabase.app/";
 include("firebaseRDB.php");
 
-function login($db, $email, $password) {
-    print("Login");
-        try {
-            // Retrieve user data from Firebase
-            $user_data = $db->retrieve("user");
-            $user_data = json_decode($user_data, true);
-
-                $result = $db->retrieve("user");
-                $result = json_decode($result, 1);
-
-                foreach ($result as $user) {
-                    if ($user['email'] == $email && $user['password'] == $password) {
-                        $_SESSION['user_id'] = $user['user_id'];
-                        header("Location: index.php");
-                    die;
-                    }
-                }
-                echo "alert('Wrong email or password!')";
-        } catch (Exception $e) {
-            // Print error message
-            echo "Error: " . $e->getMessage();
-            return null;
-        }
-
-}
-
-function signup($db, $id, $username, $email, $password) {
-    print("Signup");
-
-        try {
-            // Insert data into 'user' node
-            $insert = $db->insert("user", [
-                "user_id" => $id,
-                "userName" => $username,
-                "userEmail" => $email,
-                "userPassword" => $password
-             ]);
-             if (!$insert) {
-                 throw new Exception("Failed to insert data into Firebase Database");
-             }
-        } catch (Exception $e) {
-            // Print error message
-            echo "Error: " . $e->getMessage();
-            return null;
-        }
-
-}
-
 function check_login($db) {
-    print("Check login");
     if (isset($_SESSION['user_id'])) {
-        print_r($_SESSION['user_id']);
         $id = $_SESSION['user_id'];
 
         try {
